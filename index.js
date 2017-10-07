d038a026472cb5782d35bf5813e97aef68ab4648
Dimitar Kumanov <dimitar_kumanov@yahoo.com>
2017-10-07 20:13:28 +0300

    Initial Commit
    Set structure skeleton

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+// Define the port to run on
+app.set('port', 3000);
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Listen for requests
+const server = app.listen(app.get('port'), function() {
+  const port = server.address().port;
+});
