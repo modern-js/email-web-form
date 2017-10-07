7a578d5ab1554cd9f7fb7ed948fd74b3b5fe20f1
Dimitar Kumanov <dimitar_kumanov@yahoo.com>
2017-10-08 02:14:53 +0300

    Log users with the file system

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
 const express = require('express');
 const path = require('path');
+const fs = require('fs');
+
+
+const bodyParser = require('body-parser');
 
 const app = express();
+
+
+app.use(bodyParser.json()); // to support JSON-encoded bodies
+app.use(bodyParser.urlencoded({ // to support URL-encoded bodies
+  extended: true,
+}));
 
 // Define the port to run on
 app.set('port', 3000);
@@ -12,4 +22,14 @@ app.use(express.static(path.join(__dirname, 'public')));
 const server = app.listen(app.get('port'), () => {
   const { port } = server.address();
   console.log(`Listening on port ${port}`);
+});
+
+app.post('/', (req, res) => {
+  console.log('log');
+  const { name, email, vcsAcc } = req.body;
+  console.log(`Name: ${name} Email: ${email} vcsAcc: ${vcsAcc}`);
+  fs.appendFile('newsletter.txt', `Name: ${name} Email: ${email} vcsAcc: ${vcsAcc} \r\n`, (err) => {
+    if (err) throw err;
+    console.log('Saved!');
+  });
 });
